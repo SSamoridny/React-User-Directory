da3c8fdc79bf9d1370ec8f0b5ac16f83c5b6c6e1
Shaun Samoridny <ssamoridny@gmail.com>
2020-11-22 14:56:16 -0700

    worked with classmates and decided on another way where I eliminated and changed some files and deleted others

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+
+function Footer(){
+    return (
+        <footer>
+            <ul className="footer">
+                <li>React User Directory</li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer
